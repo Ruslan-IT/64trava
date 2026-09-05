9920034e424a776d0e75d173e1aa8f0ac17c41a4
Rusaln-IT <ruslan.r.f@yandex.ru>
2026-09-05 13:36:52 +0500

    Update admin panel category

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="{{ asset('css/style-1.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style-3.css') }}">
 
 
 
